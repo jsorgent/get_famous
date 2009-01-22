b6d1bedf0b5515b8a9123114b40cc1b9562cf105
Phill MV <phill.mv@gmail.com>
2009-01-22 11:29:18 -0500

    Added exception handling

--- get_famous.rb
+++ get_famous.rb
@@ -5,5 +5,13 @@
 # @return whether or not you will become famous
 #
 def get_famous
-  make_friends && move_to_san_fransisco
+  begin
+    make_friends && move_to_san_fransisco
+  rescue TotalFailError
+    whats_the_worst_that_can_happen
+  end
+end
+
+def whats_the_worst_that_can_happen
+  move_back_with_parents_in_minnesota
 end
